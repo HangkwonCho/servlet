cdd3f4ce8b5205d103b392d53c859fc945debe71
HangkwonCho <vatican0411@gmail.com>
2023-03-10 19:37:56 +0900

    mvc flow 설명 적용 (SpringMemberControllerV3파일에)

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -15,6 +15,18 @@ import java.util.List;
 @RequestMapping("/springmvc/v3/members")
 public class SpringMemberControllerV3 {
 
+    /**
+     * 1. Http 요청 (고객 요청)
+     * 2. DispatcherServlet -> 핸들러 매핑 (조회)
+     * 3. DispatcherServlet -> 핸들러 어댑터 목록 (핸들러를 처리할 수 있는 핸들러 어댑터 조회)
+     * 4. call Handler by HandlerAdapter - > 핸들러 어댑터를 통해서 핸들러 호출
+     * 5. ModelAndView -> 호출된 핸들러를 통해서 ModelAndView 반환
+     * 6. ViewResolver -> viewResolver를 호출
+     * 7. View -> View 를 찾은후 반환
+     * 8. Render -> View가 Render되면서 html로 변환
+     * 9. Http 응답 (고객 응답)
+     */
+
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
