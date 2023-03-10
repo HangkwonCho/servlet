c9d7f2f74d3a514c3843ccbb7066cbb00ad53b59
HangkwonCho <vatican0411@gmail.com>
2023-03-10 19:30:50 +0900

    update

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,10 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +18,13 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
     @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
     @PostMapping("/save")
     public String save(@RequestParam("username") String username,
                        @RequestParam("age") int age,
@@ -38,7 +37,8 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @GetMapping
+    //@GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
         model.addAttribute("members", members);
